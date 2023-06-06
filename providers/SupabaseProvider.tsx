68b43dfcc41cd9a64715c09a8331af51814e619f
zak <mzhussain092@gmnail.com>
2023-06-06 10:51:56 +0530

    add providers for auth and supabase

---
+++ providers/SupabaseProvider.tsx
@@ -0,0 +1,27 @@
+﻿"use client";
+
+import { Database } from "@/types_db";
+import {
+  SupabaseClient,
+  createClientComponentClient,
+} from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
+
+import { FC, useState } from "react";
+
+interface SupabaseProviderProps {
+  children: React.ReactNode;
+}
+
+const SupabaseProvider: FC<SupabaseProviderProps> = ({ children }) => {
+  const [subabaseClient] = useState(() =>
+    createClientComponentClient<Database>()
+  );
+  return (
+    <SessionContextProvider supabaseClient={subabaseClient}>
+      {children}
+    </SessionContextProvider>
+  );
+};
+
+export default SupabaseProvider;
